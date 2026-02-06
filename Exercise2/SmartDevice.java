230760c33b1af4effcac402a78e98c0e6c85f767
Isimar Lara <misa06lara@gmail.com>
2026-02-06 17:41:54 -0500

    Organized Exercise 2 into folder

--- Exercise2/SmartDevice.java
+++ Exercise2/SmartDevice.java
@@ -3,4 +3,12 @@ package Exercise2;
 public abstract class SmartDevice implements Powerable {
     protected String deviceName;
     protected boolean isOn;
+    protected static int activeDevicesCount = 0;
+
+    public SmartDevice(String deviceName) {
+        this.deviceName = deviceName;
+        this.isOn = false;
+    }
+
+    public abstract void performSelfDiagnostic();
 }
