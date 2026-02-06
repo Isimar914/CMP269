d7cea25565c776bf3d08d3325baf9cd89776b8a2
Isimar Lara <misa06lara@gmail.com>
2026-02-06 18:08:48 -0500

    Organized Exercise 2 into folder

--- Exercise2/SmartLight.java
+++ Exercise2/SmartLight.java
@@ -3,4 +3,41 @@ package Exercise2;
 public class SmartLight extends SmartDevice implements Adjustable {
     
     private int brightness;
+
+    public SmartLight(String deviceName) {
+        super(deviceName);
+        this.brightness = 0;
+    }
+
+    @Override
+    public void turnOn() {
+        if (!isOn) {
+            isOn = true;
+            activeDevicesCount++;
+        }
+    }
+
+    @Override
+    public void turnOff() {
+        if (isOn) {
+            isOn = false;
+            activeDevicesCount--;
+        }
+    }
+
+    @Override
+    public void setLevel(int level) {
+        if (isOn) {
+            if (level >= 0 && level <= 100){
+                brightness = level;
+            }
+        } else {
+            System.out.println("Cannot adjust: Device is OFF.");
+        }
+    }
+
+    @Override
+    public void performSelfDiagnostic() {
+        System.out.println("Checking LED health...");
+    }
 }
